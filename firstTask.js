962d65e6b1f830e697679aba220c1ee53f7744b7
robertarifin <arifinrobert2013@gmail.com>
2019-03-14 17:38:35 +0700

    done first task

---
+++ firstTask.js
@@ -0,0 +1,20 @@
+const fivaa = (number) => {
+  let maxDuplication = number;
+  let numberToDuplicate = number + 1;
+
+  for (let i = number - 1; i >= 0; i--) {
+    let result = '';
+    result = i.toString() + i.toString();
+
+    for (let j = 0; j < maxDuplication; j++) {
+      result += numberToDuplicate.toString();
+    }
+
+    console.log(result);
+    numberToDuplicate--;
+    maxDuplication--;
+  }
+}
+
+fivaa(5);
+fivaa(7);
